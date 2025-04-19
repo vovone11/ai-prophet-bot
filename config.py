b3afee8f609ae8a11e1f7d2166b437f7cca3ce8e
vovone11 <113759867+vovone11@users.noreply.github.com>
2025-04-20 00:27:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-
+from dotenv import load_dotenv
+import os
 import os
 
 TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 DB_PATH = "ai_prophet.db"
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
